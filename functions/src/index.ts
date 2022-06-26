ba57b317eac97cbebcb4cba4ab33202bc8e3d3b2
rtokkie <natto.and.eggs@gmail.com>
2022-06-26 11:22:02 +0900

    set region to functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,17 @@
 import * as e from 'cors';
 import * as functions from 'firebase-functions';
 
+const functionsAtTokyo = functions.region(
+  process.env.NODE_ENV === 'production' ? 'asia-northeast1' : 'us-central1'
+);
 const cors = e({ origin: true });
 const onRequest = (fn: (req: functions.https.Request, res: functions.Response, err?: any) => any) =>
-  functions.https.onRequest((req, res) => cors(req, res, (err) => fn(req, res, err)));
+  functionsAtTokyo.https.onRequest((req, res) => cors(req, res, (err) => fn(req, res, err)));
 
 export const fn1 = onRequest((_, res) => {
   res.send({ data: 'onRequest' });
 });
 
-export const fn2 = functions.https.onCall(() => {
+export const fn2 = functionsAtTokyo.https.onCall(() => {
   return 'onCall';
 });
--- src/fire/functions/index.ts
+++ src/fire/functions/index.ts
@@ -1,6 +1,10 @@
+import { getApp } from 'firebase/app';
 import { getFunctions, httpsCallable } from 'firebase/functions';
 
-const fn = getFunctions();
+const functionsAtTokyo = getFunctions(
+  getApp(),
+  import.meta.env.PROD ? 'asia-northeast1' : undefined
+);
 
-export const fn1 = httpsCallable(fn, 'fn1');
-export const fn2 = httpsCallable(fn, 'fn2');
+export const fn1 = httpsCallable(functionsAtTokyo, 'fn1');
+export const fn2 = httpsCallable(functionsAtTokyo, 'fn2');
