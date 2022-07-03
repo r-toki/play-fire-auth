53178cc11bc5cf7255bc2d8a05e42c51162ef085
rtokkie <natto.and.eggs@gmail.com>
2022-07-03 11:34:27 +0900

    fix

---
+++ README.md
@@ -0,0 +1,11 @@
+# Firebase Auth Web
+
+- https://firebase.google.com/docs/auth/web/email-link-auth
+  - email link authentication
+- https://firebase.google.com/docs/auth/web/manage-users
+  - send a user verification email
+  - send a password reset email
+- https://firebase.google.com/docs/auth/web/phone-auth
+  - phone number
+- https://firebase.google.com/docs/auth/custom-email-handler
+  - custom email handler
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,11 +5,12 @@ const functionsAtTokyo = functions.region('asia-northeast1');
 const cors = e({ origin: true });
 const onRequest = (fn: (req: functions.https.Request, res: functions.Response, err?: any) => any) =>
   functionsAtTokyo.https.onRequest((req, res) => cors(req, res, (err) => fn(req, res, err)));
+const onCall = functionsAtTokyo.https.onCall;
 
 export const fn1 = onRequest((_, res) => {
   res.send({ data: 'onRequest' });
 });
 
-export const fn2 = functionsAtTokyo.https.onCall(() => {
+export const fn2 = onCall(() => {
   return 'onCall';
 });
